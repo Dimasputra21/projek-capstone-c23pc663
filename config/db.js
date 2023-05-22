8d77028ec96b312461e59ef2958f9ff99f9d923c
Dimasputra21 <117906995+Dimasputra21@users.noreply.github.com>
2023-05-22 10:26:54 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
-import { Sequelize } from "sequelize";
-import mysql from "mysql";
-
+// import { Sequelize } from "sequelize";
+import mysql from 'mysql';
 const db = mysql.createConnection({
-    host: 34.128.95.248,
+    host: '34.128.95.248',
     user: 'root',
     database: 'authentication_db',
     password: 'dimas21032004'
-})
+});
 
 export default db;
