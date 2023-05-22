5bfe506218b1f9cd5933ab8bf7fe440cf53158a7
Dimasputra21 <117906995+Dimasputra21@users.noreply.github.com>
2023-05-22 10:36:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import db from "./config/db.js";
+// import db from "./config/db.js";
 import cors from "cors";
 import router from "./routes/route.js";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
 
-try {
-    await db.authenticate();
-    console.log('Database Tersambung ....')
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     await db.authenticate();
+//     console.log('Database Tersambung ....')
+// } catch (error) {
+//     console.error(error);
+// }
 
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log("Response success")
+    res.send("Response Success!")
+});
+
 app.use(router);
 
-app.listen(5000, () => {
-    console.log('Server berjalan pada http://localhost:5000');
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log("Server is up and listening on " + PORT)
 });
